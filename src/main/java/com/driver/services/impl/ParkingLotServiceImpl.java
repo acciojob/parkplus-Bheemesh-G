69140e0f1099bdc7c52774818751a4bd27fe388b
Bheemesh <gbheemesh888.com>
2023-03-25 17:22:09 +0530

    Done8

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -60,10 +60,6 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public void deleteSpot(int spotId) {
 
-        Spot spot = spotRepository1.findById(spotId).get();
-
-        ParkingLot parkingLot = spot.getParkingLot();
-        List<Spot> list = parkingLot.getSpotList();
       //  list.remove(spot);
         spotRepository1.deleteById(spotId);
 
